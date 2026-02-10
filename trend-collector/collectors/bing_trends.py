9430e80ace5c73d7fbd728440b356027e3a787ca
KELundy <kevin@kevinlundy.net>
2026-02-09 21:59:55 -0700

    Create bing_trends.py

---
+++ trend-collector/collectors/bing_trends.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+def fetch_bing_trends() -> List[str]:
+    """
+    Placeholder Bing search trends collector.
+
+    TODO:
+    - Integrate with Bing search/trends API.
+    - Focus on local + life-transition + real estate queries.
+
+    For now, we return static sample data.
+    """
+    return [
+        "denver real estate advisor for families",
+        "help selling parents home colorado",
+        "steps to sell a house in probate",
+    ]
